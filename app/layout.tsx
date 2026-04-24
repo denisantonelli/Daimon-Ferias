5929084fce6a5a797ed6cf2df62b3b9ba957922d
denisantonelli <denis@daimon.com.br>
2026-04-24 10:07:48 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Sistema de Férias Daimon",
+  description: "Controle de férias interno",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0, fontFamily: "Arial" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
