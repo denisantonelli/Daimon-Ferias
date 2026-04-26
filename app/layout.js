e72d640fa80b47b236b1aa551f34588054b7b846
denisantonelli <denis@daimon.com.br>
2026-04-26 08:46:54 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,14 +5,10 @@ export const metadata = {
   description: "Controle de férias interno",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body style={{ margin: 0, fontFamily: "Arial" }}>
+      <body>
         {children}
       </body>
     </html>
