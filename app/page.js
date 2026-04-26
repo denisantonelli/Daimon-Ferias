82cd495e172c66c7cc22503541ba0c47767c329e
denisantonelli <denis@daimon.com.br>
2026-04-26 08:47:22 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{padding: 40, fontFamily: 'Arial'}}>
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
       <h1>Sistema de Férias - Daimon</h1>
       <p>Seu sistema já está rodando 🚀</p>
     </main>
-  )
+  );
 }
